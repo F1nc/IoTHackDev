85fb320c4a4cb9ec1d4499e3ecb44ffe04faa696
F1nc <Keshayloc@gmail.com>
2019-04-09 22:55:42 +0300

    NOW

--- greenhouse/models.py
+++ greenhouse/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Users(models.Model):
+    _id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+
+    def __getid__(self):
+        return self.id
+'''
+    По аналогии надо саздать классы под каждую таблицу:
+        3 вида теплиц
+        юзеры
+        что там еще
+'''
